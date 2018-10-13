b040c328156b3cecf7ca803885c181e6493e8098
loukotaj <jacobloukota@gmail.com>
2018-10-13 15:09:57 -0400

    yolo

--- pillbuddy/dashboard/views.py
+++ pillbuddy/dashboard/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db import models
 from . import mqtt
+from dashboard.models import Drug
 
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    advil = Drug.objects.get(name='advil')
+    return render(request, 'dashboard.html', {'drug':advil})
 
 def dispense(request, slot):
     print("Dispense slot:", slot)
