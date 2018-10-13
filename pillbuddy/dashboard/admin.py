d4d7b942f76c9ff9ce21e5fcc5497718597fb85b
Austin Evans <austinjevans@austins-mbp.local>
2018-10-13 18:15:32 -0400

    admin

--- pillbuddy/dashboard/admin.py
+++ pillbuddy/dashboard/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Drug
+
+@admin.register(Drug)
+class RFIDAdmin(admin.ModelAdmin):
+    list_display = ('name', 'short_desc', 'per_day', 'pillsTaken')
+    fields = ['name', 'short_desc', 'per_day', 'pillsTaken']
