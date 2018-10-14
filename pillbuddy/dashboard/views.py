8e6a8f0144b7cf63a1532a42352793059198021c
loukotaj <jacobloukota@gmail.com>
2018-10-14 00:47:40 -0400

    s

--- pillbuddy/dashboard/views.py
+++ pillbuddy/dashboard/views.py
@@ -32,21 +32,4 @@ def dispense(request, slot, amount):
     d.pillsTaken += amount
     d.save()
     mqtt.publish_dispense_now(slot, amount)
-    drug1 = Drug.objects.get(slot = 1)
-    drug2 = Drug.objects.get(slot = 2)
-    drug3 = Drug.objects.get(slot = 3)
-    a = Schedule.objects.all()
-    send = None
-    drug = None
-    for item in a:
-        if item.next_dispense is not None:
-            if item.next_dispense < datetime.now():
-                send = item
-    if send is not None:
-        if send.frequency == 'weekly':
-            send.next_dispense = send.next_dispense+timedelta(days=7)
-        else:
-            send.next_dispense = send.next_dispense+timedelta(days=1)
-        send.save()
-        drug = {'slot' : Drug.objects.get(name = send.drug).slot}
-    return render(request, 'dashboard.html', {'drug1':drug1,'drug2':drug2,'drug3':drug3,'notify':send,'slot':drug})
+    return HttpResponse("")
